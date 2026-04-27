483f83f8e89673bce6d3970b6523f186cfa2dc4b
Feruz <153690859+Feruz2404@users.noreply.github.com>
2026-04-27 10:36:22 +0500

    Fix BrowserRouter future prop syntax

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,9 +4,14 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
 import './index.css';
 
+const routerFutureConfig = {
+  v7_startTransition: true,
+  v7_relativeSplatPath: true,
+};
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter future= v7_startTransition: true, v7_relativeSplatPath: true >
+    <BrowserRouter future={routerFutureConfig}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
